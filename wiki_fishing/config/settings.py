d53c0dfb4b58a28b76cc7b6d79ef050c60d9f324
Kashapov Danir <epikman9457@gmail.com>
2025-04-14 22:09:20 +0300

    Hide secret (Late is better than never)

--- wiki_fishing/config/settings.py
+++ wiki_fishing/config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w9(!$pwv=%m=o^b88l5yn@rx+%6o-8xg@9pt41q)a92vvnzgu9'
+dotenv_path = os.path.join(BASE_DIR, ".env")
+load_dotenv()
+
+SECRET_KEY = str(os.getenv("SECRET_KEY"))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
